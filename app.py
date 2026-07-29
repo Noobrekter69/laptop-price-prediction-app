81bd795f35f18b1df5d85a8cf5e16f21205650cc
Harel <160704908+Noobrekter69@users.noreply.github.com>
2026-07-29 15:43:36 +0800

    29-07-2026_add correct model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 ## Load the trained model and the list of features it expects
-model = joblib.load("laptop_price_model.pkl")
+model = joblib.load("model_features.pkl")
 feature_names = list(model.feature_names_in_)
 
 ## App title and short description
